87306cd35927353c99b6d93411577bf15315d1b7
Jaindra Kumar <jaindra.kumar@gmail.com>
2022-01-24 13:12:45 +0530

    question module

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use App\Models\Industry;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class CustomerController extends Controller
 {
@@ -24,12 +26,20 @@ class CustomerController extends Controller
 #--------------------------- Insert/Edit Customer ------------------------------#   
     public function store(Request $request) {
 
-        $ValidationRules = $request->validate([
-            'customer_name' => 'required|unique:customers,customer_name',
-            'customer_email' => 'required|email|unique:customers,customer_email',
-            'customer_logo' => 'required|image|mimes:jpg,svg',
-            //'customer_zip' => 'required',
-        ]);
+        $ValidationRules = $request->validate ([
+                'customer_name' => 'required|unique:customers,customer_name',
+                'customer_email' => 'required|email|unique:customers,customer_email',
+                'customer_password' =>'required|min:10|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/',
+
+                'password_confirm'  => 'required|same:customer_password',
+                'customer_logo' => 'required|image|mimes:jpg,svg',
+            ], 
+            [
+                'customer_password.regex'  => 'Password must contain at least 1 UpperCase letter, 1 lowercase letter, 1 number, 1 Special Character.',
+                'password_confirm.same' => 'Password and Confirm Password should be same',
+                'customer_logo.required' => 'Customer logo must be a file of type : JPG & svg',
+            ]);
+
         $validator = Validator::make(Input::all(), $ValidationRules);
         if ($validator->fails()) {
             return Response::json(array('success' => false,'errors' => $validator->getMessageBag()->toArray() ), 400); 
@@ -85,5 +95,44 @@ class CustomerController extends Controller
         $customer = Customer::where('id',$request->id)->delete();
         return response()->json(['success' => true]);
     }
+
+#--------------------------- Customer Login Page --------------------------------------#
+    public function CustomerLoginForm() {
+        //login check
+        if(Auth::check()){
+            $id = Auth::user()->id;
+            $user = User::where(['id'=>$id])->first();
+            if($user->role == 'admin'){
+                return redirect('dashboard');
+            }
+            else{
+                return redirect('customer-list');
+            }
+        }
+        else{
+            return view('customer/login');
+        }
+    }
+#--------------------------- Customer Login  --------------------------------------#
+    public function CustomerLogin(Request $request) {
+        $request->validate([
+            'customer_email' => 'required',
+            'customer_password' => 'required',
+        ]);
+
+
+        if (Auth::guard('customer')->attempt(['customer_email' => $request->customer_email, 'customer_password' => $request->customer_password], $request->get('remember'))) {
+
+            return redirect()->intended('/admin');
+        }
+        return back()->withInput($request->only('email', 'remember'));
+
+
+    }
+
+#---------------------------  --------------------------------------#
+
+
+
             
 }
---
+++ resources/views/question/all-questions.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title></title>
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+      <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </head>
+
+<body>
+  <div class="page-header">
+    <h3><a href="http://localhost/start-data/question-type">Questions View</a></h3>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+    
+      <form action="{{route('survey.save')}}" method="POST" name="exam">
+        {{csrf_field()}}
+        <div class="col-md-12">
+            @foreach($questions as $key=>$ques)
+              <h5> {{$key+1}}. {{$ques->question}}</h5>
+              <ol class="ul-list"  style="list-style-type: lower-alpha;" >
+                @foreach($ques->optionsdata as $opt)
+                <li><input type="radio" name="ans{{$key+1}}" value="{{$opt->option}}" /> {{$opt->option}}   </li>
+                @endforeach
+              </ol>
+            @endforeach
+
+        </div>
+      </form>
+    </div>
+  </div>
+
+</body>
+</html>
